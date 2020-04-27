22561ca1aba1d92f4c0688f91804cfcd6d6a83a2
HEroKuma <zx8733520+github@gapp.nthu.edu.tw>
2020-04-27 16:44:58 +0900

    add torch res18 model fixxed the res model create by myself

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.models import resnet18
 
 
 class ResidualBlock(nn.Module):
@@ -103,19 +104,30 @@ class SimpleCNN(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, ResidualBlock, num_classes=4):
         super(ResNet, self).__init__()
-        self.inchannel = 64
+        self.inchannel = 32
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
         )
-        self.layer1 = self.make_layer(ResidualBlock, 64,  3, stride=1)
-        self.layer2 = self.make_layer(ResidualBlock, 128, 3, stride=2)
-        self.layer3 = self.make_layer(ResidualBlock, 256, 3, stride=2)
-        self.layer4 = self.make_layer(ResidualBlock, 512, 3, stride=2)
-        self.fc = nn.Sequential(
-            nn.Linear(2048, 512),
+        self.layer1 = self.make_layer(ResidualBlock, 32,  3, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, 64, 3, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 128, 3, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, 256, 3, stride=2)
+        self.layer5 = self.make_layer(ResidualBlock, 512, 3, stride=2)
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=3),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(256, 128, kernel_size=3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.fc = nn.Sequential(
             nn.Linear(512, 128),
             nn.ReLU(inplace=True),
             nn.Linear(128, 32),
@@ -134,11 +146,13 @@ class ResNet(nn.Module):
     def forward(self, x):
         out = self.conv1(x)
         out = self.layer1(out)
-        out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, 2)
         out = self.layer2(out)
-        out = F.avg_pool2d(out, 4)
         out = self.layer3(out)
         out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
         return out
@@ -212,3 +226,31 @@ class SimpleCNN2(nn.Module):
         #         print(x.size())
         x = self.fc(x)
         return x
+
+
+class PlantModel(nn.Module):
+
+    def __init__(self, pretrained, num_class=4):
+        super(PlantModel, self).__init__()
+        self.backbone = resnet18(pretrained=pretrained)
+        in_feature = self.backbone.fc.in_features
+        self.logit = nn.Linear(in_feature, num_class)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        x = self.backbone.conv1(x)
+        x = self.backbone.bn1(x)
+        x = self.backbone.relu(x)
+        x = self.backbone.maxpool(x)
+
+        x = self.backbone.layer1(x)
+        x = self.backbone.layer2(x)
+        x = self.backbone.layer3(x)
+        x = self.backbone.layer4(x)
+
+        x = self.backbone.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+
+        out = self.logit(x)
+        return out
