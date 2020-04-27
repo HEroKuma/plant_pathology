f2ad2496146d9e30405e4e1a6165a70f78085575
HEroKuma <zx8733520+github@gapp.nthu.edu.tw>
2020-04-27 16:43:58 +0900

    use n_fold to split and train change the transpose function

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ from DataLoader import *
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from torch.utils.data.sampler import SubsetRandomSampler
+from sklearn.model_selection import StratifiedKFold
 
 from torch.utils.data import DataLoader
 from torchvision import datasets
@@ -28,15 +29,40 @@ transforms_2 = [transforms.Resize((256, 256), Image.BICUBIC),
                    transforms.ToTensor(),
                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
 
-transforms_ = transforms_2
+transforms_3 = [
+    transforms.CenterCrop(512),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(5),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0, 0, 0], std=[.3, .2, .1])
+]
+
+transforms_ = transforms_3
+
+
+class DenseCrossEntropy(nn.Module):
+    def __init__(self):
+        super(DenseCrossEntropy, self).__init__()
+
+    def forward(self, y_pred, y_true):
+        y_pred = y_pred.float()
+        y_true = y_true.float()
+
+        logprobs = torch.log_softmax(y_pred, dim=-1)
+        # print(logprobs, y_true)
+        loss = -y_true * logprobs
+        loss = torch.sum(loss)
+
+        return loss.mean()
+
 
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('--epoch', type=int, default=0)
     parser.add_argument('--n_epochs', type=int, default=100)
-    parser.add_argument('--batch', type=int, default=4)
+    parser.add_argument('--batch', type=int, default=2)
     parser.add_argument('--duration', type=int, default=10)
-    parser.add_argument('--lr', type=float, default=0.005)
+    parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--n_cpu', type=int, default=4)
     parser.add_argument('--gpu_num', type=str, default='1')
     opt = parser.parse_args()
@@ -50,66 +76,72 @@ def train(opt):
 
     # model = SimpleCNN2().cuda()
     model = ResNet18().cuda()
+    optimizer = torch.optim.SGD(model.parameters(), lr=opt.lr, momentum=.9, weight_decay=5e-4)
+    criterion = nn.CrossEntropyLoss()
 
     data = Data('./', transforms_)
+    """
     data_size = len(data)
     indices = list(range(data_size))
     split = int(np.floor(.2*data_size))
     np.random.seed(42)
     np.random.shuffle(indices)
     train_indices, val_indices = indices[split:], indices[:split]
+    """
+    all_data = pd.read_csv('./train.csv')
+    d = all_data.iloc[:, 0].values
+    skf = StratifiedKFold(5, shuffle=True, random_state=42)
+    targets = all_data.iloc[:, [1, 2, 3, 4]].values
+    train_y = targets[:, 0] + targets[:, 1]*2+targets[:, 2]*3
 
-    train_set = SubsetRandomSampler(train_indices)
-    val_set = SubsetRandomSampler(val_indices)
+    for i_fold, (t, v) in enumerate(skf.split(d, train_y)):
+        train_index = SubsetRandomSampler(t)
+        val_index = SubsetRandomSampler(v)
+        train_loader = torch.utils.data.DataLoader(data, batch_size=opt.batch, sampler=train_index, num_workers=opt.n_cpu)
+        val_loader = torch.utils.data.DataLoader(data, batch_size=opt.batch, sampler=val_index, num_workers=opt.n_cpu)
+        with tqdm.tqdm(total=opt.epoch, miniters=1, mininterval=0) as progress:
+            val_min = float('inf')
+            for epoch in range(opt.n_epochs//5):
+                model.train()
+                test_correct = 0
+                for i, batch in enumerate(train_loader, 1):
+                    images = Variable(batch['X']).cuda()
+                    correct_label = Variable(batch['Y']).cuda()
 
-    train_loader = torch.utils.data.DataLoader(data, batch_size=opt.batch, sampler=train_set, num_workers=opt.n_cpu)
-    val_loader = torch.utils.data.DataLoader(data, batch_size=opt.batch, sampler=val_set, num_workers=opt.n_cpu)
+                    optimizer.zero_grad()
+                    outputs = model(images)
+                    _, label = torch.max(outputs, 1)
+                    correct_num = (correct_label == label).sum()
+                    test_correct += correct_num.item()
+                    acc = test_correct/(correct_label.size(0)*i)
+                    loss = criterion(outputs, correct_label)
+                    loss.backward()
+                    optimizer.step()
 
-    # optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr)
-    optimizer = torch.optim.SGD(model.parameters(), lr=opt.lr, momentum=.9, weight_decay=5e-4)
-    criterion = nn.CrossEntropyLoss()
+                    if i % 5 == 0:
+                        progress.set_description(
+                            "fold: {fold}, epoch: {epoch}, Iter {iter:.1f}%, Loss: {loss:.4f}, acc: {acc:.2f}%"\
+                            .format(fold=i_fold, epoch=epoch, iter=100*(i+1)/len(train_loader),\
+                            loss=loss, acc=acc*100)
+                        )
 
-    with tqdm.tqdm(total=opt.epoch, miniters=1, mininterval=0) as progress:
-        val_min = float('inf')
-        for epoch in range(opt.n_epochs):
-            model.train()
-            test_correct = 0
-            for i, batch in enumerate(train_loader, 1):
-                images = Variable(batch['X']).cuda()
-                correct_label = Variable(batch['Y']).cuda()
-
-                optimizer.zero_grad()
-                outputs = model(images)
-                _, label = torch.max(outputs, 1)
-                correct_num = (correct_label == label).sum()
-                test_correct += correct_num.item()
-                acc = test_correct/(correct_label.size(0)*i)
-                loss = criterion(outputs, correct_label)
-                loss.backward()
-                optimizer.step()
-
-                if i % 5 == 0:
-                    progress.set_description("epoch: {epoch}, Iter {iter:.1f}%, Loss: {loss:.4f}, acc: {acc:.2f}%}"\
-                                             .format(epoch=epoch, iter=100*(i+1)/len(train_loader),\
-                                             loss=loss, acc=acc*100))
-
-            model.eval()
-            correct = 0
-            test_loss = 0
-            for j, batch in enumerate(val_loader):
-                images = Variable(batch['X']).cuda()
-                correct_label = Variable(batch['Y']).cuda()
-                outputs = model(images)
-                _, label = torch.max(outputs, 1)
-                correct += (correct_label == label).sum()
-                loss = criterion(outputs, correct_label)
-                test_loss += (opt.batch*loss.item())
-            print("\tTest Loss: {loss:.4f}, acc: {acc:.2f}%".format(loss=test_loss/len(val_loader)\
-                                                                   , acc=100*correct/(opt.batch*len(val_loader))),)
-            if test_loss < val_min:
-                val_min = test_loss
-                torch.save(model.state_dict(), 'Res18')
-                print('\t==Model Saved==')
+                model.eval()
+                correct = 0
+                test_loss = 0
+                for j, batch in enumerate(val_loader):
+                    images = Variable(batch['X']).cuda()
+                    correct_label = Variable(batch['Y']).cuda()
+                    outputs = model(images)
+                    _, label = torch.max(outputs, 1)
+                    correct += (correct_label == label).sum()
+                    loss = criterion(outputs, correct_label)
+                    test_loss += (opt.batch*loss.item())
+                print("\tTest Loss: {loss:.4f}, acc: {acc:.2f}%".format(loss=test_loss/len(val_loader)\
+                                                                       , acc=100*correct/(opt.batch*len(val_loader))),)
+                if test_loss < val_min:
+                    val_min = test_loss
+                    torch.save(model.state_dict(), 'Res18')
+                    print('\t==Model Saved==')
 
     predict(model)
 
