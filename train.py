cd4affdcd61899f1ccbb8d10582e7263a6b32cbc
HEroKuma <zx8733520+github@gapp.nthu.edu.tw>
2020-04-29 04:13:34 +0900

    softmax+cross entropy

--- train.py
+++ train.py
@@ -37,6 +37,12 @@ transforms_3 = [
     transforms.Normalize(mean=[0, 0, 0], std=[.3, .2, .1])
 ]
 
+test_trans = [
+    transforms.CenterCrop(512),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0, 0, 0], std=[0.3, 0.2, 0.1])
+]
+
 transforms_ = transforms_3
 
 
@@ -47,7 +53,10 @@ class DenseCrossEntropy(nn.Module):
     def forward(self, y_pred, y_true):
         y_pred = y_pred.float()
         y_true = y_true.float()
-
+        output = torch.zeros((len(y_true), 4)).cuda()
+        for i in range(len(y_true)):
+            output[i][int(y_true[i].item())] = 1
+        y_true = output
         logprobs = torch.log_softmax(y_pred, dim=-1)
         # print(logprobs, y_true)
         loss = -y_true * logprobs
@@ -59,9 +68,8 @@ class DenseCrossEntropy(nn.Module):
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('--epoch', type=int, default=0)
-    parser.add_argument('--n_epochs', type=int, default=100)
-    parser.add_argument('--batch', type=int, default=2)
-    parser.add_argument('--duration', type=int, default=10)
+    parser.add_argument('--n_epochs', type=int, default=30)
+    parser.add_argument('--batch', type=int, default=32)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--n_cpu', type=int, default=4)
     parser.add_argument('--gpu_num', type=str, default='1')
@@ -75,9 +83,12 @@ def train(opt):
     os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_num
 
     # model = SimpleCNN2().cuda()
-    model = ResNet18().cuda()
-    optimizer = torch.optim.SGD(model.parameters(), lr=opt.lr, momentum=.9, weight_decay=5e-4)
-    criterion = nn.CrossEntropyLoss()
+    model = PlantModel(True).cuda()
+    # model = ResNet18().cuda()
+    optimizer = torch.optim.SGD(model.parameters(), lr=opt.lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=.95)
+    # criterion = nn.CrossEntropyLoss()
+    criterion = DenseCrossEntropy()
 
     data = Data('./', transforms_)
     """
@@ -90,9 +101,11 @@ def train(opt):
     """
     all_data = pd.read_csv('./train.csv')
     d = all_data.iloc[:, 0].values
-    skf = StratifiedKFold(5, shuffle=True, random_state=42)
+    skf = StratifiedKFold(5, shuffle=True, random_state=412)
     targets = all_data.iloc[:, [1, 2, 3, 4]].values
     train_y = targets[:, 0] + targets[:, 1]*2+targets[:, 2]*3
+    val_min = float('inf')
+    model_name = ''
 
     for i_fold, (t, v) in enumerate(skf.split(d, train_y)):
         train_index = SubsetRandomSampler(t)
@@ -100,16 +113,15 @@ def train(opt):
         train_loader = torch.utils.data.DataLoader(data, batch_size=opt.batch, sampler=train_index, num_workers=opt.n_cpu)
         val_loader = torch.utils.data.DataLoader(data, batch_size=opt.batch, sampler=val_index, num_workers=opt.n_cpu)
         with tqdm.tqdm(total=opt.epoch, miniters=1, mininterval=0) as progress:
-            val_min = float('inf')
-            for epoch in range(opt.n_epochs//5):
+            for epoch in range(opt.n_epochs):
                 model.train()
                 test_correct = 0
                 for i, batch in enumerate(train_loader, 1):
                     images = Variable(batch['X']).cuda()
                     correct_label = Variable(batch['Y']).cuda()
-
                     optimizer.zero_grad()
                     outputs = model(images)
+                    # print(outputs)
                     _, label = torch.max(outputs, 1)
                     correct_num = (correct_label == label).sum()
                     test_correct += correct_num.item()
@@ -118,11 +130,11 @@ def train(opt):
                     loss.backward()
                     optimizer.step()
 
-                    if i % 5 == 0:
+                    if i % 4 == 0:
                         progress.set_description(
-                            "fold: {fold}, epoch: {epoch}, Iter {iter:.1f}%, Loss: {loss:.4f}, acc: {acc:.2f}%"\
+                            "fold: {fold}, epoch: {epoch}, Iter {iter:.1f}%, Loss: {loss:.4f}, acc: {acc:.2f}%, lr: {lr:.5f}"\
                             .format(fold=i_fold, epoch=epoch, iter=100*(i+1)/len(train_loader),\
-                            loss=loss, acc=acc*100)
+                            loss=loss, acc=acc*100, lr=scheduler.get_lr()[0])
                         )
 
                 model.eval()
@@ -131,24 +143,30 @@ def train(opt):
                 for j, batch in enumerate(val_loader):
                     images = Variable(batch['X']).cuda()
                     correct_label = Variable(batch['Y']).cuda()
-                    outputs = model(images)
-                    _, label = torch.max(outputs, 1)
-                    correct += (correct_label == label).sum()
-                    loss = criterion(outputs, correct_label)
-                    test_loss += (opt.batch*loss.item())
-                print("\tTest Loss: {loss:.4f}, acc: {acc:.2f}%".format(loss=test_loss/len(val_loader)\
-                                                                       , acc=100*correct/(opt.batch*len(val_loader))),)
+                    with torch.no_grad():
+                        outputs = model(images)
+                        _, label = torch.max(outputs, 1)
+                        correct += (correct_label == label).sum()
+                        loss = criterion(outputs, correct_label)
+                    test_loss += loss.item()
+                print("\tTest Loss: {loss:.4f}, acc: {acc:.2f}% {corr}/{data_size}".format(loss=test_loss/len(val_loader)\
+                                                                       , acc=100*correct/(opt.batch*len(val_loader)),\
+                                                                        corr=correct, data_size=len(val_index))
+                                                                        )
                 if test_loss < val_min:
                     val_min = test_loss
-                    torch.save(model.state_dict(), 'Res18')
-                    print('\t==Model Saved==')
+                    # torch.save(model.state_dict(), 'Res18')
+                    torch.save(model, 'Res18_epoch.pth'.format(fold=i_fold, epoch=epoch))
+                    model_name = 'Res18_epoch.pth'.format(fold=i_fold, epoch=epoch)
+                    print('\t==Model Saved with {fold}-{epoch}=='.format(fold=i_fold, epoch=epoch))
+                scheduler.step()
 
-    predict(model)
+    predict(model_name)
 
 
-def predict(model):
-    testLoader = DataLoader(testData('./', transforms_), batch_size=1, num_workers=2)
-    # model.eval()
+def predict(name):
+    testLoader = DataLoader(testData('./', test_trans), batch_size=1, num_workers=2)
+    model = torch.load(name).cuda()
 
     with open('output.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
@@ -156,13 +174,14 @@ def predict(model):
         for i, batch in enumerate(testLoader):
             predict_array = [batch[1][0], 0, 0, 0, 0]
             images = Variable(batch[0]).cuda()
-            outputs = model(images)
-            _, label = torch.max(outputs, 1)
-            predict_array[label.item()+1] = 1
+            with torch.no_grad():
+                outputs = model(images)
+                _, label = torch.max(outputs, 1)
+                predict_array[label.item()+1] = 1
             writer.writerow(predict_array)
-
 
 
 if __name__ == '__main__':
     opt = parse()
     train(opt)
+    # predict('Res18')
